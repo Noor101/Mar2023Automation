b5c20aef22e89d75f663fd205e16e5206441faec
unknown <noormohammed1050@gmail.com>
2023-03-02 11:23:07 +0530

    changes done as per the review

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,10 +1,13 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage {
 	
 	String value = "name";
+	private By checkout = By.name("checkout");
 	
-	public void getUserName() {
+	public void getUserProduct() {
 		System.out.println("please get the user name");
 	}
 
