9b0156ea74b280781a41dad72b5e1a426d635ef4
unknown <noormohammed1050@gmail.com>
2023-03-03 19:14:17 +0530

    fixed login test cases and deleted unwanted files

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class Constants {
 	
-	public static final String LOGIN_PAGE_TITLE = "Account Login11";
-	public static final String ACCOUNTS_PAGE_TITLE = "My Account12";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
+	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
 	public static final String LOGIN_PAGE_URL_FRACTION = "route=account/login";
 	public static final int DEFAULT_TIME_OUT = 5;
 	public static final int MACBOOK_PRO_IMAGE_COUNT = 4;
